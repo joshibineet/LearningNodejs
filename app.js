b547218b90081f3a91aa8446ea87dcaec0b36352
joshibineet <joshibineet123@gmail.com>
2023-09-24 22:52:19 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const a = 20;
+const b = 10;
+// console.log(a)
+// console.log(b)
+const name = "Bineet"
+console.log(name)
+const sum = a + b 
+console.log(sum)
+// console.log("Welcome to Nodejs")
+
+// ..... Array .......
+const number =[10,29,30]
+// console.log(number[1])
+
+// to calculate lenght
+console.log(number.length)
+
+//.. objects ............
+
+const person = {
+    name : "Bineet",
+    age : 23
+}
+console.log("My name is" + person.name)
+console.log(person.age)
